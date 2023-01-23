23af8f7b2f925f77f0f556987332b1c971dbccd7
Vitor Sousa <v.sousa.cf@gmail.com>
2023-01-23 06:28:01 -0300

    HomePage: create sections

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import SideIcon from '../../components/SideIcon';
-import { Container, SideBar } from './styles';
+import Arrow from '../../assets/icons/arrow.svg';
+import { Container, Content, Header, SideBar, Summary } from './styles';
 
 const Home = () => {
   return (
@@ -20,6 +21,56 @@ const Home = () => {
           <SideIcon type="LogOff" />
         </div>
       </SideBar>
+
+      <Content>
+        <Header>
+          <div>
+            <h1>Página inicial</h1>
+            <small>Acompanhe todas as suas finanças</small>
+          </div>
+          <small>X</small>
+        </Header>
+
+        <Summary>
+          <h1>Resumo da sua vida financeira</h1>
+          <div className="containerSummary">
+            <div>
+              <h3>Minhas receitas</h3>
+              <strong>R$12.253,70</strong>
+            </div>
+            <div>
+              <h3>Minhas despesas</h3>
+              <strong>R$12.253,70</strong>
+            </div>
+            <div>
+              <h3>Meus cartões</h3>
+              <strong>R$12.253,70</strong>
+            </div>
+          </div>
+        </Summary>
+
+        <div className="containerSections">
+          <section>
+            <header>
+              <h1>Últimas transações</h1>
+              <button>
+                <small>Ver todas</small>
+                <img src={Arrow} alt="Arrow" />
+              </button>
+            </header>
+          </section>
+
+          <section>
+            <header>
+              <h1>Meus cartões</h1>
+              <button>
+                <small>Ver todas</small>
+                <img src={Arrow} alt="Arrow" />
+              </button>
+            </header>
+          </section>
+        </div>
+      </Content>
     </Container>
   );
 };
--- client/src/pages/Home/styles.ts
+++ client/src/pages/Home/styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+`;
 
 export const SideBar = styled.aside`
   box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.25);
@@ -15,8 +17,10 @@ export const SideBar = styled.aside`
   width: 3.5rem;
   height: 100vh;
 
+  position: sticky;
+
   h1 {
-    font-size: 3rem;
+    font-size: 2.5rem;
     font-weight: 800;
     span {
       font-weight: 400;
@@ -28,5 +32,89 @@ export const SideBar = styled.aside`
     flex-direction: column;
     align-items: center;
     gap: 4rem;
+  }
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 2rem;
+    font-weight: 400;
+  }
+`;
+
+export const Content = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  padding: 3.5rem 2rem;
+  gap: 2rem;
+  flex: 1;
+
+  section {
+    display: flex;
+    flex-direction: column;
+    padding: 1.5rem;
+    gap: 1.5rem;
+    width: 100%;
+
+    border: 1px solid var(--gray-200);
+    border-radius: 12px;
+
+    h1 {
+      font-weight: 700;
+      font-size: 32px;
+    }
+  }
+
+  .containerSections {
+    display: flex;
+    align-items: flex-start;
+    gap: 2rem;
+
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      button {
+        background: transparent;
+        border: none;
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+
+        small {
+          font-size: 1rem;
+          color: var(--black-500);
+        }
+      }
+    }
+  }
+`;
+
+export const Summary = styled.section`
+  h3 {
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  h3,
+  strong {
+    color: var(--black-700);
+  }
+
+  .containerSummary {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+    div + div {
+      border-left: solid 2px var(--gray-700);
+      padding-left: 1rem;
+    }
   }
 `;
