53a69ab8697c2d1424b862fbcbf098de66b05196
Vitor Sousa <v.sousa.cf@gmail.com>
2023-01-25 16:53:49 -0300

    LoginPage: create hooks useErrors and implementing verify in the LoginPage

---
+++ client/src/hooks/useErrors.ts
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+interface ErrorsProps {
+  field: string;
+  message: string;
+}
+
+export default function useErrors() {
+  const [errors, setErrors] = useState<ErrorsProps[]>([]);
+
+  function setError({ field, message }: ErrorsProps) {
+    const errorAlreadyExists = errors.find((error) => error.field === field);
+
+    if (errorAlreadyExists) {
+      return;
+    }
+
+    setErrors((prevState) => [...prevState, { field, message }]);
+  }
+
+  function removeError(fieldName: string) {
+    setErrors((prevState) =>
+      prevState.filter((error) => error.field !== fieldName)
+    );
+  }
+
+  function getErrorMessageByFieldName(fieldName: string) {
+    return errors.find((error) => error.field === fieldName)?.message;
+  }
+
+  return {
+    errors,
+    setError,
+    removeError,
+    getErrorMessageByFieldName,
+  };
+}
--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,27 +1,71 @@
-import React from 'react';
+import React, { BaseSyntheticEvent, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '../../components/Button';
 import Input from '../../components/Input';
 import Logo from '../../components/Logo';
+import useErrors from '../../hooks/useErrors';
+import isEmailValid from '../../utils/isEmailValid';
 import { Container } from './styles';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { errors, getErrorMessageByFieldName, removeError, setError } =
+    useErrors();
+
   const navigate = useNavigate();
+  const isFormValid = password && email && errors.length === 0;
+
+  function handleEmailChange(event: BaseSyntheticEvent) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      return setError({ field: 'email', message: 'E-mail inválido' });
+    } else {
+      removeError('email');
+    }
+  }
+
+  function handlePasswordChange(event: BaseSyntheticEvent) {
+    setPassword(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: 'password', message: 'Senha é obrigatória' });
+    } else {
+      removeError('password');
+    }
+  }
 
   function handleSubmit(event: React.BaseSyntheticEvent) {
     event.preventDefault();
 
     navigate('/home');
   }
+
   return (
     <Container>
       <Logo />
       <form onSubmit={handleSubmit}>
         <h1>Faça seu login</h1>
-        <Input category="email" placeholder="Seu E-mail" type="email" />
-        <Input category="password" placeholder="Sua senha" type="password" />
+        <Input
+          category="email"
+          placeholder="Seu E-mail"
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+          error={getErrorMessageByFieldName('email')}
+        />
 
-        <Button variant="primary" type="submit">
+        <Input
+          category="password"
+          placeholder="Sua senha"
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          error={getErrorMessageByFieldName('password')}
+        />
+
+        <Button variant="primary" type="submit" disabled={!isFormValid}>
           Fazer Login
         </Button>
         <Link to="/home">Página home</Link>
