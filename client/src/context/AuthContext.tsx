73efd3a609737f348cde7b01091bac403a1bd349
Vitor Sousa <v.sousa.cf@gmail.com>
2023-01-28 18:22:22 -0300

    Hooks: create hook useAuth

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,13 +1,6 @@
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
-import { redirect, useNavigate } from 'react-router-dom';
+import { createContext, ReactNode, useContext } from 'react';
+import useAuth from '../hooks/useAuth';
 import { User } from '../interface/User';
-import { api } from '../services/api';
 
 interface AuthContextProviderProps {
   children: ReactNode;
@@ -26,41 +19,8 @@ export const AuthContext = createContext<AuthContextProps>(
 );
 
 export function AuthProvider({ children }: AuthContextProviderProps) {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [userId, setUserId] = useState('');
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-
-    if (token) {
-      api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
-      setAuthenticated(true);
-    }
-
-    setLoading(false);
-  });
-
-  async function handleLogin(user: User) {
-    try {
-      const { data } = await api.post('/auth/authenticate', user);
-
-      localStorage.setItem('token', JSON.stringify(data.token));
-      api.defaults.headers.Authorization = `Bearer ${data.token}`;
-      setUserId(data.user._id);
-      setAuthenticated(true);
-    } catch (error: any) {
-      const errorMessage = JSON.parse(error.request.response);
-
-      return errorMessage;
-    }
-  }
-
-  function handleLogout() {
-    setAuthenticated(false);
-    localStorage.removeItem('token');
-    api.defaults.headers.Authorization = null;
-  }
+  const { authenticated, loading, userId, handleLogin, handleLogout } =
+    useAuth();
 
   return (
     <AuthContext.Provider
---
+++ client/src/hooks/useAuth.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { User } from '../interface/User';
+import { api } from '../services/api';
+
+export default function useAuth() {
+  const [authenticated, setAuthenticated] = useState(false);
+  const [userId, setUserId] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+      setAuthenticated(true);
+    }
+
+    setLoading(false);
+  });
+
+  async function handleLogin(user: User) {
+    try {
+      const { data } = await api.post('/auth/authenticate', user);
+
+      localStorage.setItem('token', JSON.stringify(data.token));
+      api.defaults.headers.Authorization = `Bearer ${data.token}`;
+      setUserId(data.user._id);
+      setAuthenticated(true);
+    } catch (error: any) {
+      const errorMessage = JSON.parse(error.request.response);
+
+      return errorMessage;
+    }
+  }
+
+  function handleLogout() {
+    setAuthenticated(false);
+    localStorage.removeItem('token');
+    api.defaults.headers.Authorization = null;
+  }
+
+  return { handleLogin, handleLogout, loading, authenticated, userId };
+}
