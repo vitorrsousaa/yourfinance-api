54398581a68620701613285966b665fd9bcc44e6
Vitor Sousa <v.sousa.cf@gmail.com>
2023-02-01 10:17:19 -0300

    Header: created Header component

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,7 +1,19 @@
 import { Container } from './styles';
+import Arrow from '../../assets/icons/arrow.svg';
 
 const Header = () => {
-  return <Container>Bem vindo de volta Natalia Nunes</Container>;
+  return (
+    <Container>
+      <h1>
+        Bem vindo(a) de volta, <span>Natalia Nunes</span>
+      </h1>
+      <div className="containerAvatar">
+        <img src="https://picsum.photos/200/300" alt="ImageAvatar" />
+        <h4>Natalia Nunes</h4>
+        <img src={Arrow} alt="Arrow" />
+      </div>
+    </Container>
+  );
 };
 
 export default Header;
--- client/src/components/Header/styles.ts
+++ client/src/components/Header/styles.ts
@@ -1,3 +1,33 @@
 import styled from 'styled-components';
 
-export const Container = styled.header``;
+export const Container = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 1.5rem;
+    font-weight: 700;
+
+    color: var(--black-800);
+    span {
+      font-weight: 400;
+    }
+  }
+
+  .containerAvatar {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    h4 {
+      font-size: 1rem;
+    }
+
+    img:first-child {
+      width: 56px;
+      height: 56px;
+      border-radius: 100%;
+    }
+  }
+`;
