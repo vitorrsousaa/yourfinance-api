0835998207e3bbd8b543d042f8fd27c108901a30
Vitor Sousa <v.sousa.cf@gmail.com>
2023-02-16 17:29:39 -0300

    Theme: Create a globalTheme

---
+++ client/src/assets/styles/theme/index.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from 'styled-components';
+
+const colors = {
+  primary: '#195f',
+};
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#195f',
+  },
+  teste: {
+    color: colors.primary,
+  },
+};
+
+export default theme;
