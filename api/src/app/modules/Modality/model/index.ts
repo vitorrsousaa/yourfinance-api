f47528a2352f4be500e59435f8153d6430e5bbd2
Vitor Sousa <v.sousa.cf@gmail.com>
2023-03-16 16:47:03 -0300

    Increase category in modality model

--- api/src/app/modules/Modality/model/index.ts
+++ api/src/app/modules/Modality/model/index.ts
@@ -1,7 +1,8 @@
-import { HydratedDocument, model, Schema } from 'mongoose';
+import { HydratedDocument, model, Schema, Types } from 'mongoose';
 
 type ModalityInfos = {
   name: string;
+  category: Types.ObjectId;
 };
 
 export type TModality = HydratedDocument<ModalityInfos>;
@@ -10,6 +11,11 @@ const ModalitySchema = new Schema<TModality>({
   name: {
     type: String,
     require: true,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    require: true,
+    ref: 'Category',
   },
 });
 
