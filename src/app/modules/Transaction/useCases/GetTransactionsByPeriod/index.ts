25e9ddd0636991d7e0bd7fda2c1f34a04b713c93
Joao Lucas <mainexcalibur@protonmail.com>
2023-03-25 16:54:37 -0300

    refactor: removing the error when the user has no transaction made in some period

--- src/app/modules/Transaction/useCases/GetTransactionsByPeriod/index.ts
+++ src/app/modules/Transaction/useCases/GetTransactionsByPeriod/index.ts
@@ -1,10 +1,8 @@
-import AppError from '../../../../error';
 import { TTransaction } from '../../model';
 import TransactionRepository from '../../repositories/implementation/TransactionRepository';
 
 export default async function GetTransactionsByPeriod(period: string | number, userId: string): Promise<TTransaction[] | null> {
   const findTransactions = await TransactionRepository.findByPeriod(userId, String(period));
-  if (!findTransactions) throw new AppError('You no have transactions!', 404);
 
   return findTransactions;
 }
