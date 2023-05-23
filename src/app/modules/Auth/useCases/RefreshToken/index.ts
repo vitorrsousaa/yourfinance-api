7b531aec9a9c411a2430fccc4c164214538a84a6
Vitor Sousa <v.sousa.cf@gmail.com>
2023-05-23 19:35:47 -0300

    Modified returning to refresh route

--- src/app/modules/Auth/useCases/RefreshToken/index.ts
+++ src/app/modules/Auth/useCases/RefreshToken/index.ts
@@ -1,17 +1,22 @@
-import { ACCESS_TOKEN_EXPIRATION } from '../../../../constants/auth';
+import {
+  ACCESS_TOKEN_EXPIRATION,
+  REFRESH_TOKEN_EXPIRATION,
+} from '../../../../constants/auth';
 import AppError from '../../../../error';
 import generateToken from '../../../../utils/generateToken';
 import UserRepository from '../../../User/repositories/implementations/UserRepository';
 
 export default async function RefreshToken(
   userId: string
-): Promise<{ token: string }> {
+): Promise<{ access: string; refresh: string }> {
   const findUser = await UserRepository.findById(userId);
   if (!findUser) throw new AppError('User not found!', 404);
 
-  const refreshToken = generateToken(findUser.id, ACCESS_TOKEN_EXPIRATION);
+  const accessToken = generateToken(findUser.id, ACCESS_TOKEN_EXPIRATION);
+  const refreshToken = generateToken(findUser.id, REFRESH_TOKEN_EXPIRATION);
 
   return {
-    token: refreshToken,
+    refresh: refreshToken,
+    access: accessToken,
   };
 }
