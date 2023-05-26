3caf67e61eee2087ca6d28392fc494a67070bf88
Vitor Sousa <v.sousa.cf@gmail.com>
2023-05-25 21:43:31 -0300

    bugfix/transactions: Includes category and modality when returning transactions

--- src/app/modules/Transaction/repositories/implementation/TransactionRepository.ts
+++ src/app/modules/Transaction/repositories/implementation/TransactionRepository.ts
@@ -1,43 +1,63 @@
 import { itemsPerPage } from '../../../../constants/pagination';
 import { TCategory } from '../../../../entities/category/TCategory';
+import {
+  TransactionCreateRequestDTO,
+  TransactionGetRequestDTO,
+} from '../../../../entities/transaction/dtos';
 import { TTransaction } from '../../../../entities/transaction/TTransaction';
 import prisma from '../../../../prisma';
-import { ITransactionRepository, TReturnTransactionsWithCategoryAndModality } from '../ITransactionRepository';
-import { TransactionCreateRequestDTO, TransactionGetRequestDTO } from '../../../../entities/transaction/dtos';
+import {
+  ITransactionRepository,
+  TReturnTransactionsWithCategoryAndModality,
+} from '../ITransactionRepository';
 
 class TransactionRepository implements ITransactionRepository {
-  async findTransactionByIdUserAndPage({id, page}: TransactionGetRequestDTO<number>): Promise<TReturnTransactionsWithCategoryAndModality[] | null> {
+  async findTransactionByIdUserAndPage({
+    id,
+    page,
+  }: TransactionGetRequestDTO<number>): Promise<
+    TReturnTransactionsWithCategoryAndModality[] | null
+  > {
     const skipPages = (page as number) * itemsPerPage;
 
     return prisma.transaction.findMany({
       where: {
-        userId: id
+        userId: id,
       },
       include: {
         Category: true,
-        Modality: true
+        Modality: true,
       },
       orderBy: {
-        date: 'desc'
+        date: 'desc',
       },
       skip: skipPages,
-      take: itemsPerPage
+      take: itemsPerPage,
     });
   }
 
   async findById(id: string): Promise<TTransaction | null> {
     return prisma.transaction.findUnique({
       where: {
-        id
-      }
+        id,
+      },
+      include: {
+        Category: true,
+        Modality: true,
+      },
     });
   }
 
-  async findByPeriod({id, month}: TransactionGetRequestDTO<string>): Promise<TReturnTransactionsWithCategoryAndModality[] | null> {
+  async findByPeriod({
+    id,
+    month,
+  }: TransactionGetRequestDTO<string>): Promise<
+    TReturnTransactionsWithCategoryAndModality[] | null
+  > {
     const endDate = new Date();
 
     const year = endDate.getFullYear();
-    const lastMonths = endDate.getMonth() - parseInt((month as string));
+    const lastMonths = endDate.getMonth() - parseInt(month as string);
     const day = endDate.getDay();
 
     const startDate = new Date(year, lastMonths, day);
@@ -47,31 +67,36 @@ class TransactionRepository implements ITransactionRepository {
         userId: id,
         date: {
           gte: startDate,
-          lte: endDate
-        }
+          lte: endDate,
+        },
       },
       include: {
         Category: true,
-        Modality: true
-      }
+        Modality: true,
+      },
     });
   }
 
-  async findByDateAgo({id, date}: TransactionGetRequestDTO<Date>): Promise<TReturnTransactionsWithCategoryAndModality[] | null> {
+  async findByDateAgo({
+    id,
+    date,
+  }: TransactionGetRequestDTO<Date>): Promise<
+    TReturnTransactionsWithCategoryAndModality[] | null
+  > {
     const endDate = new Date();
 
     return prisma.transaction.findMany({
       where: {
         userId: id,
         date: {
-          gte: (date as Date),
-          lte: endDate
-        }
+          gte: date as Date,
+          lte: endDate,
+        },
       },
       include: {
         Category: true,
-        Modality: true
-      }
+        Modality: true,
+      },
     });
   }
 
@@ -83,7 +108,7 @@ class TransactionRepository implements ITransactionRepository {
     userId,
     amount,
     date,
-    informationFixedId
+    informationFixedId,
   }: TransactionCreateRequestDTO): Promise<TTransaction> {
     return prisma.transaction.create({
       data: {
@@ -94,16 +119,20 @@ class TransactionRepository implements ITransactionRepository {
         userId,
         amount,
         date,
-        informationFixedId: informationFixedId ? informationFixedId : null
-      }
+        informationFixedId: informationFixedId ? informationFixedId : null,
+      },
+      include: {
+        Category: true,
+        Modality: true,
+      },
     });
   }
 
   async delete(id: string): Promise<unknown> {
     return prisma.transaction.delete({
       where: {
-        id
-      }
+        id,
+      },
     });
   }
 
@@ -115,40 +144,46 @@ class TransactionRepository implements ITransactionRepository {
     return prisma.transaction.updateMany({
       where: {
         InformationFixed: {
-          id: idInformation
+          id: idInformation,
         },
         date: {
-          gte: dateGreaterThan
-        }
+          gte: dateGreaterThan,
+        },
       },
       data: {
-        amount: newValueAmount
-      }
+        amount: newValueAmount,
+      },
     });
   }
 
-  async deleteManyTransactionWithTimeGreaterThan(idInformation: string, dateGreaterThan: Date): Promise<unknown> {
+  async deleteManyTransactionWithTimeGreaterThan(
+    idInformation: string,
+    dateGreaterThan: Date
+  ): Promise<unknown> {
     return prisma.transaction.deleteMany({
       where: {
         informationFixedId: idInformation,
         date: {
-          gte: dateGreaterThan
-        }
-      }
+          gte: dateGreaterThan,
+        },
+      },
     });
   }
 
-  async findAllTransactionsByUser(userId: string): Promise<(TTransaction & { Category: TCategory })[] | null> {
+  async findAllTransactionsByUser(
+    userId: string
+  ): Promise<(TTransaction & { Category: TCategory })[] | null> {
     return prisma.transaction.findMany({
       where: {
-        userId
+        userId,
       },
       orderBy: {
-        date: 'desc'
+        date: 'desc',
       },
       include: {
-        Category: true
-      }
+        Category: true,
+        Modality: true,
+      },
     });
   }
 }
