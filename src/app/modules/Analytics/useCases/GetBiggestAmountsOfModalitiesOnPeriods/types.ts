79f7a95319fffdb00568c774681ad84747522511
Vitor Sousa <vitor.silva@vetta.digital>
2023-05-29 20:39:20 -0300

    fix: Added category how to optional item

--- src/app/modules/Analytics/useCases/GetBiggestAmountsOfModalitiesOnPeriods/types.ts
+++ src/app/modules/Analytics/useCases/GetBiggestAmountsOfModalitiesOnPeriods/types.ts
@@ -1,16 +1,15 @@
-
 export type TObjModality = {
   id: string;
   name: string;
-  category: string;
+  category?: string;
   amount: number;
-}
+};
 
 export type TB = {
   [key: string]: {
     modality: TObjModality[];
     added?: boolean;
-  }
+  };
   0: {
     modality: TObjModality[];
     added?: boolean;
@@ -18,13 +17,13 @@ export type TB = {
   3: {
     modality: TObjModality[];
     added?: boolean;
-  }
+  };
   6: {
     modality: TObjModality[];
     added?: boolean;
-  }
+  };
   12: {
     modality: TObjModality[];
     added?: boolean;
-  }
-}
+  };
+};
